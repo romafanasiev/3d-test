ba036f2e887ba0b099fde51b57890be36feb4e06
Roman Afanasiev <roman.afanasiev@chisw.com>
2024-01-02 16:09:45 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,24 @@
 'use client';
-
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Environment, OrbitControls } from '@react-three/drei';
 import { Test } from './models';
-import dynamic from 'next/dynamic';
-
-const ZapparCameraSSR = dynamic(
-  () => import('@zappar/zappar-react-three-fiber/lib/Components/ZapparCamera'),
-  {
-    ssr: false,
-  }
-);
+import { ARButton, XR } from '@react-three/xr';
 
 export default function Home() {
-  const [ar, setAr] = useState(false);
-
   return (
     <main>
-      <div className="h-screen w-screen relative">
-        {ar ? (
-          <Canvas>
-            <ZapparCameraSSR environmentMap poseMode="anchor-origin" />
-            <Suspense fallback={null}>
-              <Test />
-            </Suspense>
-          </Canvas>
-        ) : (
-          <Canvas>
+      <div className="h-screen w-screen">
+        <ARButton />
+        <Canvas>
+          <XR>
             <Suspense fallback={null}>
               <Test scale={[10, 10, 10]} />
               <Environment preset="sunset" background />
               <OrbitControls autoRotate />
             </Suspense>
-          </Canvas>
-        )}
-        <button
-          className="absolute bottom-1 left-[50%] p-2 bg-white color-black z-10"
-          onClick={() => setAr(!ar)}
-        >
-          AR
-        </button>
+          </XR>
+        </Canvas>
       </div>
     </main>
   );
